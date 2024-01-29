f5e1dd08154547b660e8aaf5dae61f8003572e77
okatsn <okatsn@gmail.com>
2024-01-29 06:30:36 +0000

    Add CI for sending email & Project dependencies.

---
+++ scripts/weekly_summary.jl
@@ -0,0 +1,10 @@
+using AccountingBook, DataFrames
+using Statistics
+using CSV
+using Chain
+using PrettyTables
+using HypertextLiteral
+using Markdown
+using Suppressor
+using Test
+using SMTPClient
