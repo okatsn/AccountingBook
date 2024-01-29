e60134829217e429fd2042053adbca2baf52f448
Documenter.jl <documenter@juliadocs.github.io>
2024-01-29 05:58:49 +0000

    build based on b41e713

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = AccountingBook","category":"page"},{"location":"#AccountingBook","page":"Home","title":"AccountingBook","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for AccountingBook.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [AccountingBook]","category":"page"}]
+}
