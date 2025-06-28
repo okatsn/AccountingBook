5b820e9815b754cc6e2f91407c1744d63f69ce0e
okatsn <okatsn@gmail.com>
2025-06-28 15:44:56 +0800

    A new sheet for summary visualization.

---
+++ scripts/visual_summaries.jl
@@ -0,0 +1,17 @@
+using AccountingBook, DataFrames
+using Statistics
+using CSV
+using Chain
+using PrettyTables
+using HypertextLiteral
+using Markdown
+using Test
+using SMTPClient
+using Dates
+
+sheetid = ARGS[1]
+
+url = "https://docs.google.com/spreadsheets/d/$sheetid/edit?usp=sharing"
+df0 = readgsheet(url)
+
+df = preparesheet(df0)
