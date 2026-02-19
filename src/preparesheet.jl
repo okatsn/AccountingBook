ab9acbd137ac46b7ed835459bb2d16577dc8ee47
okatsn <okatsn@gmail.com>
2026-02-19 15:41:53 +0800

    Fix column rename.

--- src/preparesheet.jl
+++ src/preparesheet.jl
@@ -24,13 +24,6 @@ function preparesheet2(df0a)
         # trasform(Cols(r"^(IN|OUT)_") => ByRow():in_or_out)
         rename!("時間戳記" => :timestr,
             "電子郵件地址" => :email,
-            "誰" => :whosaccount,
-            "資產類別" => :assettype,
-            "項目" => :item,
-            "數量" => :quantity,
-            "單價" => :unit_price,
-            "單價單位" => :unit_unit,
-            "備註" => :memo,
         )
     end
 
@@ -38,7 +31,16 @@ function preparesheet2(df0a)
     for direction in ["IN", "OUT"]
         expr = Regex("$(direction)_")
         dftmp = select(df0a, :timestr, :email, Cols(expr)) |> emptyprefix!(expr)
+        rename!(dftmp,
+            "誰" => :whosaccount,
+            "資產類別" => :assettype,
+            "項目" => :item,
+            "數量" => :quantity,
+            "單價" => :unit_price,
+            "單價單位" => :unit_unit,
+            "備註" => :memo,)
         insertcols!(dftmp, :inout => direction)
+
         push!(dfas, dftmp)
     end
 
