2443d5bcad8018cd9b6fc325ba8b120bf9a33145
okatsn <okatsn@gmail.com>
2026-02-19 15:21:51 +0800

    Rename columns.

--- src/preparesheet.jl
+++ src/preparesheet.jl
@@ -6,6 +6,7 @@ function preparesheet(df0)
             "支出或收入" => :inout,
             "從誰的口袋" => :whosaccount,
             "金額" => :amount,
+            "幣別" => :unit_currency,
             "備註" => :memo,)
     end
 end
@@ -22,7 +23,15 @@ function preparesheet2(df0a)
     @chain df0a begin
         # trasform(Cols(r"^(IN|OUT)_") => ByRow():in_or_out)
         rename!("時間戳記" => :timestr,
-            "電子郵件地址" => :email)
+            "電子郵件地址" => :email,
+            "誰" => :whosaccount,
+            "資產類別" => :assettype,
+            "項目" => :item,
+            "數量" => :quantity,
+            "單價" => :unit_price,
+            "單價單位" => :unit_unit,
+            "備註" => :memo,
+        )
     end
 
     dfas = DataFrame[]
